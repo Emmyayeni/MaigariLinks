cb42335c709d6dcb540744b73096aaf2d88c07c1
Emmyayeni <learnpythonmessage@gmail.com>
2026-04-21 17:13:16 +0100

    iaui

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+// avatar component based on radix-ui/react-avatar with some custom styling and fallback support
 import * as React from 'react'
 import * as AvatarPrimitive from '@radix-ui/react-avatar'
 
